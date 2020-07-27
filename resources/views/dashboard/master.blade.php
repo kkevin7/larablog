3e128b0394f618d13979b20640f1a9d3254b41c2
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-07-27 14:17:30 -0600

    delete with modal

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -13,7 +13,6 @@
     <div class="container">
         @yield('content')
     </div>
-
+    <script src="{{asset("/js/app.js")}}"></script>
 </body>
-<script src="{{asset("/js/app.js")}}"></script>
 </html>
--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -31,17 +31,52 @@
                     <a href="{{route('post.edit',$post->id)}}" class="btn btn-primary">Edit</a>
                 </td>
                 <td>
-                    <form method="POST" action="{{route('post.destroy',$post->id)}}">
-                        @method('delete')
-                        @csrf
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                    </form>
+                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal" data-id="{{$post->id}}">Delete </button>
                 </td>
             </tr>
             @endforeach
         </tbody>
     </table>
 </div>
+
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="deleteModalLabel">Delete Post</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure that do you want to delete the record?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+                <form id="formDelete" method="POST" action="{{route('post.destroy',0)}}" data-action="{{route('post.destroy',0)}}">
+                    @method('delete')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    window.onload = function() {
+        $('#deleteModal').on('show.bs.modal', function(event) {
+            var button = $(event.relatedTarget) // Button that triggered the modal
+            var id = button.data('id') // Extract info from data-* attributes
+            // var modal = $(this)
+            action = $('#formDelete').attr('data-action').slice(0, -1);
+            action += id;
+            $("#formDelete").attr('action', action);
+            // modal.find('.modal-body input').val(recipient)
+        });
+    }
+
+</script>
 
 @include('dashboard.partials.session-flash-status')
 @include('dashboard.partials.validation-error')
