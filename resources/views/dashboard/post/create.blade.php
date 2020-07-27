a16de6e90434f2a9f9eac183939a463456861d85
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-07-27 12:08:53 -0600

    remender the content of the filds

--- resources/views/dashboard/post/create.blade.php
+++ resources/views/dashboard/post/create.blade.php
@@ -6,18 +6,18 @@
         @csrf
         <div class="form-group">
             <label for="title">Titulo</label>
-            <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="" aria-describedby="helpId">
-            @error('title')
+            <input type="text" name="title" id="title" value="{{ old('title') }}" class="form-control @error('title') is-invalid @enderror" placeholder="" aria-describedby="helpId">
+            {{-- @error('title')
             <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
+            @enderror --}}
         </div>
         <div class="form-group">
             <label for="url_clean">URL Clean</label>
-            <input type="text" name="url_clean" id="url_clean" class="form-control" placeholder="" aria-describedby="helpId">
+            <input type="text" name="url_clean" id="url_clean" value="{{ old('url_clean') }}" class="form-control  @error('url_clean') is-invalid @enderror" placeholder="" aria-describedby="helpId">
         </div>
         <div class="form-group">
             <label for="content">Centenido</label>
-            <textarea class="form-control" name="content" id="content" rows="3"></textarea>
+            <textarea class="form-control  @error('content') is-invalid @enderror" name="content" id="content" rows="3">{{ old('content') }}</textarea>
         </div>
         <input class="btn btn-primary" type="submit" value="Enviar">
 
